695f7eae4f2c851de919c9322f70b747de1454cb
MarcusAfolabi <biormarcus@gmail.com>
2023-10-30 02:26:52 +0100

    update locally

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -14,93 +14,27 @@ use Illuminate\Support\Facades\Session;
 use Illuminate\Validation\Rules\Password;
 
 class AccountController extends Controller
-{
- 
-    private function baseUrl()
-    {
-        return config('app.url');
-    }
-
-    private function header()
-    {
-        $token = Session::get('token');
-
-        $headers = [
-            // "Authorization" => "Bearer $token",
-            "Accept" => "application/json",
-            "Content-Type" => "application/json",
-        ];
-
-        return $headers;
-    } 
-
-    // public function postLogin(Request $request)
-    // {
-    //     $email = $request->get('email');
-    //     $password = $request->get('password');
-        
-    //     $body = [
-    //         'email' => $email,
-    //         'password' => $password,
-    //     ];
-        
-    //     $generate = $this->baseUrl() . '/login';
-    //     $response = Http::withHeaders($this->header())->post($generate, $body);
-    //     dd($response);
-
-    //     if ($response->successful()) {
-    //         $login = $response->json();
-    //         dd($login);
-    //     }
-
-    //     // if (Auth::attempt($authuser)) {
-    //     //     return redirect()->intended(route('dashboard'));
-    //     // } else {
-    //     //     return redirect(route('account.login'))->with('error', 'Hey, that detail is wrong. Try again');
-    //     // }
-    // }
+{    
     public function postLogin(Request $request)
     {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
-        $user = User::find(Auth::guard('user')->user()->id);
-
-        if (Auth::attempt($user)) {
-            $tokenn = bin2hex(openssl_random_pseudo_bytes(64));
-            $token = base64_encode($tokenn . $user->email);
-            $now = Carbon::now()->toDateTimeString();
-            User::where('email', $user->email)->update(['device_token' => $token, 'token_time' => $now]);
-
+    
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = Auth::user();
+    
             if ($user->status == 'inactive') {
-                return response()->json([
-                    "status" => 401,
-                    "message" => "Oops, your account has been deactivated! Please contact Admin via the chatbot."
-                ]);
+                return redirect()->route('login')->with('status', 'Oops, your account has been deactivated! Please contact Admin via the chatbot.');
             }
-
-
-            $wallet = Wallet::where('user_id', $user->id)->first();
-            $balance = $wallet->balance;
-            $wallet_code = $wallet->code;
-
-            return response()->json([
-                "status" => 200,
-                "message" => "Successful",
-                "token" => $token,
-                "user" => $user,
-                "wallet" => $wallet,
-                "balance" => number_format($balance, 2),
-                "wallet_code" => $wallet_code,
-            ], 200);
-        } else {
-            return response()->json([
-                "status" => 401,
-                "message" => "Incorrect Email or Password!"
-            ]);
+    
+            return redirect()->intended(route('dashboard.index'));
         }
+    
+        return redirect()->route('login')->with('status', 'Hey, Sorry, I could not recognize your details.');
     }
+    
 
     public function index()
     {
@@ -143,25 +77,25 @@ class AccountController extends Controller
         }
     }
 
-    
+
     public function show(Account $account)
     {
         //
     }
 
-  
+
     public function edit(Account $account)
     {
         //
     }
 
-   
+
     public function update(Request $request, Account $account)
     {
         //
     }
 
-  
+
     public function destroy(Account $account)
     {
         $account->delete();
@@ -169,7 +103,7 @@ class AccountController extends Controller
     }
 
     public function logout(Request $request)
-    { 
+    {
         Session::flush();
         Auth::logout();
         return redirect(route('account.login'))->with('status', 'Logout successful');
