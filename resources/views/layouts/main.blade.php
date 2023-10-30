c020621f8c01b8d5da0a991463a98060d3932c5f
MarcusAfolabi <biormarcus@gmail.com>
2023-10-30 03:09:53 +0100

    workingupdate locally

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -102,12 +102,14 @@
                             <div class="flex items-start">
                                 <div class="flex-shrink-0">
                                     <!-- Heroicon name: outline/check-circle -->
-                                    <svg class="h-6 w-6 text-red-800" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-red-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z" />
                                     </svg>
+
                                 </div>
                                 <div class="ml-3 w-0 flex-1 pt-0.5">
-                                    <p class="text-sm font-medium text-red-900">Oops!</p>
+                                    <p class="text-sm font-medium text-red-500">Oops!</p>
                                     <p class="mt-1 text-sm text-purple-600">{{ session('error') }}</p>
                                 </div>
                                 <div class="ml-4 flex-shrink-0 flex">
